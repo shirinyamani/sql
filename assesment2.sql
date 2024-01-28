5b1a21526ff6cf6c0880459550c6df89a1a372b2
Shirin Yamani <shirinyamani@shirin.ucalgary.ca>
2024-01-28 15:46:47 -0700

    assement 2 completed

--- assesment2.sql
+++ assesment2.sql
@@ -50,4 +50,30 @@ WHERE EXTRACT(YEAR FROM starttime) = 2012 AND EXTRACT(MONTH FROM starttime) = 9
 GROUP BY facid
 ORDER BY Total_slot;
 
+-- 12
+SELECT facid, SUM(slots) AS total_slots
+FROM cd.bookings 
+GROUP BY facid
+HAVING SUM(slots) >1000
+ORDER BY facid;
+
+-- 13
+SELECT cd.bookings.starttime AS start,
+cd.facilities.name AS name
+FROM cd.facilities
+INNER JOIN cd.bookings 
+ON cd.bookings.facid = cd.facilities.facid
+WHERE cd.facilities.facid IN (0,1) 
+AND cd.bookings.starttime >='2012-09-21'
+AND cd.bookings.starttime < '2012-09-22'
+ORDER BY starttime;
+
+--14
+SELECT cd.bookings.starttime 
+FROM cd.bookings 
+INNER JOIN cd.members ON 
+cd.members.memid = cd.bookings.memid 
+WHERE cd.members.firstname='David' 
+AND cd.members.surname='Farrell';
+
 
