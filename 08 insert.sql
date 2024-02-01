4977ffc625f1eba41595fd37e5b368562ea199e6
Shirin Yamani <shirinyamani@shirin.ucalgary.ca>
2024-02-01 11:20:29 -0700

    delte added

--- 08 insert.sql
+++ 08 insert.sql
@@ -2,3 +2,20 @@
 INSERT INTO accout (username, password, email, created_on)
 VALUES
 ('Shirin','password', 'shirin@mail.com', CURRENT_TIMESTAMP)
+
+
+-- UPDATE
+UPDATE accout
+SET  last_login = CURRENT_TIMESTAMP 
+WHERE last_login IS null
+
+UPDATE account_job
+SET hire_date = accout.created_on
+FROM accout
+WHERE account_job.user_id = accout.user_id
+
+
+-- DELETE
+DELETE FROM account_job
+WHERE user_id = 1
+
